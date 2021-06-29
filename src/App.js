5fbd0082750db618eec810e028d9500097760e97
mdenizsert <e170503010@stud.tau.edu.tr>
2021-06-29 16:09:05 +0300

    Button component created. Small styling works has been done.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import Header from "./components/Header";
-import React from "react";
+
 const App= () => {
 
 
   return (
-    <div className="container">
+    <div className='container'>
       <Header />
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,22 @@
-
-const Header = () => {
+import PropTypes from 'prop-types'
+import Button from './Button'
+const Header = ({title}) => {
     return (
-        <Header>
-            <h1>Hello From React</h1>
-        </Header>
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button color='green' text='Add'/>
+        </header>
     )
 }
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+
 
 export default Header
