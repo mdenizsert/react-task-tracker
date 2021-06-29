13e77a61731e985608454cce443b01e63655bd5d
mdenizsert <e170503010@stud.tau.edu.tr>
2021-06-29 18:03:34 +0300

    Toggle Reminder added.

--- src/App.js
+++ src/App.js
@@ -20,10 +20,18 @@ const deleteTask = (id) => {
 }
 
 
+//Toggle Reminder
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) => task.id === id
+  ? {...task,reminder:
+    !task.reminder} : task))
+}
+
+
   return (
     <div className='container'>
       <Header />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}/> : 'No Tasks To Show'}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks To Show'}
     </div>
   );
 }
