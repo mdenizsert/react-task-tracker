617640858ed0d3edebcc6372de4740effcce0365
mdenizsert <e170503010@stud.tau.edu.tr>
2021-06-29 16:50:52 +0300

    Task and Tasks components created.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import {useState} from 'react'
 
 const App= () => {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5',
+        reminder: true,
+    }
+    
+
+])
 
 
   return (
     <div className='container'>
       <Header />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
