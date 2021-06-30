35710df5e9270c45571e873eb7673f4ca2994db4
mdenizsert <e170503010@stud.tau.edu.tr>
2021-06-30 10:23:18 +0300

    Toggle reminder integreted to seerver.

--- src/App.js
+++ src/App.js
@@ -31,6 +31,16 @@ const App= () => {
 
 
 
+    const fetchTask = async (id) => {
+      const res = await fetch(`http://localhost:5000/tasks/${id}`)
+      const data = await res.json()
+
+      return data
+
+    }
+
+
+
 
 
 //Add Task
@@ -65,10 +75,26 @@ const deleteTask = async (id) => {
 
 
 //Toggle Reminder
-const toggleReminder = (id) => {
+const toggleReminder = async (id) => {
+
+  const taskToToggle = await fetchTask(id)
+  const updTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+
+  const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+    method:'PUT',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(updTask)
+  })
+
+  const data = await res.json()
+
+
+
   setTasks(tasks.map((task) => task.id === id
   ? {...task,reminder:
-    !task.reminder} : task))
+    data.reminder} : task))
 }
 
 
