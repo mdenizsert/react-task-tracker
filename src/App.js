ddfe805e8ab13caf9dfd5ab629ae7c858b0f5917
mdenizsert <e170503010@stud.tau.edu.tr>
2021-06-30 10:53:06 +0300

    About and Footer components created.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import {useState, useEffect} from 'react'
 import AddTask from "./components/AddTask";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import { BrowserRouter as Router,  Route} from "react-router-dom";
 
 const App= () => {
   const[showAddTask, setShowAddTask] = useState(false)
@@ -99,11 +102,20 @@ const toggleReminder = async (id) => {
 
 
   return (
+    <Router>
     <div className='container'>
       <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
-      {showAddTask && <AddTask onAdd={addTask} />}
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks To Show'}
+      
+      <Route path='/' exact render={(props) => (
+        <>
+        {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : ('No Tasks To Show')}
+        </>
+      )} />
+      <Route path= '/about' component={About} />
+      <Footer />
     </div>
+    </Router>
   );
 }
 
